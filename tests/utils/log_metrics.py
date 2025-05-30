4b3bc2675a7e6ed5c2e9f21502300c8c45db0ca6
Yifan Sun <Y.sun-44@student.tudelft.nl>
2025-05-31 01:55:39 +0200

    debug

--- tests/utils/log_metrics.py
+++ tests/utils/log_metrics.py
@@ -1,13 +1,20 @@
-import json
-import os
+def log_metric(name, value, message=None, precision=3, category=None, json_path="metrics.json"):
+    import os, json
+    from pathlib import Path
 
-def log_metric(name, value, message=None, precision=3,  category=None, json_path="metrics.json"):
+    json_path = os.path.abspath(json_path)
+
     if isinstance(value, float):
         value = round(value, precision)
+    elif isinstance(value, bool):
+        value = str(value).lower()  
 
     if os.path.exists(json_path):
-        with open(json_path, "r") as f:
-            metrics = json.load(f)
+        try:
+            with open(json_path, "r") as f:
+                metrics = json.load(f)
+        except json.JSONDecodeError:
+            metrics = {} 
     else:
         metrics = {}
 
